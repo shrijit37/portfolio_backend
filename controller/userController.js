55bac05755e2a0d522229242c37ada4d2eef3a4e
shrijit37 <shrijitsrivastav@gmail.com>
2024-11-28 09:12:32 +0530

    for backend deployment

---
+++ controller/userController.js
@@ -0,0 +1,216 @@
+import { v2 as cloudinary } from "cloudinary";
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import { User } from "../models/userSchema.js";
+import ErrorHandler from "../middlewares/error.js";
+import { generateToken } from "../utils/jwtToken.js";
+import crypto from "crypto";
+import { sendEmail } from "../utils/sendEmail.js";
+
+
+
+
+
+export const register = catchAsyncErrors(async (req, res, next) => {
+
+  console.log(req.body);
+
+  const {
+    fullName,
+    email,
+    phone,
+    aboutMe,
+    password,
+    githubURL,
+    instagramURL,
+    twitterURL,
+    linkedInURL,
+  } = req.body;
+
+  const user = await User.create({
+    fullName,
+    email,
+    phone,
+    aboutMe,
+    password,
+    githubURL,
+    instagramURL,
+    twitterURL,
+    linkedInURL,
+  });
+    try {
+      generateToken(user, "Registered!", 201, res);
+      // newUser = await user.save();
+      // console.log(newUser);
+    } catch (error) {
+      console.log(error.message);
+    }
+});
+
+
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Provide Email And Password!", 400));
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email Or Password!", 404));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email Or Password", 401));
+  }
+  generateToken(user, "Login Successfully!", 200, res);
+});
+
+
+
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Logged Out!",
+    });
+});
+
+
+
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+
+
+
+
+
+export const updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    fullName: req.body.fullName,
+    email: req.body.email,
+    phone: req.body.phone,
+    aboutMe: req.body.aboutMe,
+    githubURL: req.body.githubURL,
+    instagramURL: req.body.instagramURL,
+    twitterURL: req.body.twitterURL,
+    linkedInURL: req.body.linkedInURL,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Profile Updated!",
+    user,
+  });
+});
+
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const { currentPassword, newPassword, confirmNewPassword } = req.body;
+  const user = await User.findById(req.user.id).select("+password");
+  if (!currentPassword || !newPassword || !confirmNewPassword) {
+    return next(new ErrorHandler("Please Fill All Fields.", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(currentPassword);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Incorrect Current Password!"));
+  }
+  if (newPassword !== confirmNewPassword) {
+    return next(
+      new ErrorHandler("New Password And Confirm New Password Do Not Match!")
+    );
+  }
+  user.password = newPassword;
+  await user.save();
+  res.status(200).json({
+    success: true,
+    message: "Password Updated!",
+  });
+});
+
+export const getUserForPortfolio = catchAsyncErrors(async (req, res, next) => {
+  const id = "663296a896e553748ab5b0be";
+  const user = await User.findById(id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+//FORGOT PASSWORD
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorHandler("User Not Found!", 404));
+  }
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${process.env.DASHBOARD_URL}/password/reset/${resetToken}`;
+
+  const message = `Your Reset Password Token is:- \n\n ${resetPasswordUrl}  \n\n If 
+  You've not requested this email then, please ignore it.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Personal Portfolio Dashboard Password Recovery`,
+      message,
+    });
+    res.status(201).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+//RESET PASSWORD
+export const resetPassword = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Reset password token is invalid or has been expired.",
+        400
+      )
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password & Confirm Password do not match"));
+  }
+  user.password = await req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  generateToken(user, "Reset Password Successfully!", 200, res);
+});
---
+++ utils/jwtToken.js
@@ -0,0 +1,20 @@
+export const generateToken = async(user, message, statusCode, res) => {
+  const token = user.generateJsonWebToken();
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      // expires: new Date(
+      //   Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      // ),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message,
+      user,
+      token,
+    });
+    // const newUser =  await user.save();
+    // console.log(newUser);
+};
+
